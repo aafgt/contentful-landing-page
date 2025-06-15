7f253022d8972f23da13435faf5cb1356de61a6a
Amir Ashraf <aafouad9@gmail.com>
2025-06-15 21:31:13 +0300

    style fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from "../util/types";
 
 const Card: React.FC<CardProps> = ({ title, category, image, cta, price }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-3 flex max-w-1/2">
+    <div className="bg-white rounded-lg shadow-lg p-3 flex lg:max-w-1/2">
       <div className="space-y-2 w-2/3">
         <p className="bg-yellow-400 rounded-full px-3 py-0.5 font-semibold w-fit">{category}</p>
         <h5 className="font-semibold text-2xl">{title}</h5>
