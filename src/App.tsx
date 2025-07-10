f230b5c34f91483f60ee0ccaee6f8eaba8995592
Amir Ashraf <aafouad9@gmail.com>
2025-07-10 12:16:09 +0300

    added live-preview security

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ProductPage from "./pages/ProductPage";
+import { useEffect, useState } from "react";
 
 const router = createBrowserRouter([
   {
@@ -20,11 +21,34 @@ const router = createBrowserRouter([
 function App() {
   // Render the ContentfulLivePreviewProvider to enable live preview and inspector mode.
   // Dynamically render each section from Contentful using getReactComponentOfSection.
+
+  const [isAllowed, setIsAllowed] = useState(true);
+
+  const isLivePreview =
+    new URLSearchParams(window.location.search).get("preview") === "true";
+
+  useEffect(() => {
+    if (isLivePreview) {
+      document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+      const isInIframe = window.self !== window.top;
+
+      if (!isInIframe) {
+        setIsAllowed(false);
+      }
+    }
+  }, [isLivePreview]);
+
+  if (!isAllowed) {
+    return <p>Access Denied.</p>;
+  }
+
   return (
     <ContentfulLivePreviewProvider
       locale="en-US"
       enableLiveUpdates
       enableInspectorMode
+      targetOrigin="https://app.contentful.com"
     >
       <RouterProvider router={router} />
     </ContentfulLivePreviewProvider>
